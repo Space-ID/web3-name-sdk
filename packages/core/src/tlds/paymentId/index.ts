1cfcdb47febb0d93a6ee177bffb018f7163e860a
stanli1231 <stan.li@space.id>
2025-03-06 16:01:15 +0800

    feat: add paymentId name service

---
+++ packages/core/src/tlds/paymentId/index.ts
@@ -0,0 +1,58 @@
+import { createPublicClient, hexToBigInt, http, keccak256, stringToHex } from "viem"
+import { paymentIdReaderAbi } from "../../abi/paymentId/paymentIdReader"
+import { PaymentIdTld, PaymentIdTldCode } from "../../constants/tld"
+// import { base } from "viem/chains"
+export const BaseTestChain = {
+    id: 84532,
+    name: 'Base Sepolia Testnet',
+    nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+    rpcUrls: {
+        default: {
+            http: ['https://sepolia.base.org/'],
+        },
+    },
+    blockExplorers: {
+        default: {
+            name: 'Basescan',
+            url: 'https://sepolia.basescan.org',
+        },
+    },
+}
+
+function getTokenIdBigint(domainName: string) {
+    // 计算 domainName 的 keccak256 哈希，并转换为 bigint
+    console.log('getTokenIdBigint', domainName)
+    const nameHash = keccak256(stringToHex(domainName.split('@')[0]))
+    return hexToBigInt(nameHash)
+}
+
+function getTldCode(domainName: string): bigint {
+    console.log('domain', domainName)
+    const parts = domainName.split('@')
+    const tld = parts[1] as PaymentIdTld
+    return BigInt(PaymentIdTldCode[tld])
+}
+
+export class PaymentIdName {
+    private client = createPublicClient({
+        chain: BaseTestChain,
+        transport: http('https://sepolia.base.org/'),
+    })
+
+    async getAddress({ name, chainId }: { name: string, chainId: number }) {
+        try {
+            const address = await this.client.readContract({
+                address: '0xcd8E5A0023Cb750B89898BdAf776e26138E76E33',
+                abi: paymentIdReaderAbi,
+                functionName: 'addr',
+                args: [getTokenIdBigint(name), getTldCode(name), BigInt(chainId)]
+
+            })
+            return address
+        }
+        catch (error) {
+            console.error('Error getting PaymentId address', error)
+            return null
+        }
+    }
+}
